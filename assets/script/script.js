8d2421b150249c08272b58c6c8bfff155dc8f0b9
alanabarbosa <itslanayall@hotmail.com>
2025-05-24 20:04:52 -0300

    Add background move position

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,12 +1,15 @@
-const buttons = document.querySelectorAll('.buttons img');
+const buttons = document.querySelectorAll('.button-background');
 const legend = document.querySelectorAll('.buttons span');
 let currentAudio = null;
 
-for (let i = 1; i <= 12; i++) legend[i - 1].innerHTML = legend[i - 1].dataset.legend;
+for (let i = 1; i <= 12; i++) {
+  legend[i - 1].innerHTML = legend[i - 1].dataset.legend;
+}
 
 const getSound = (event) => {
-    const audio = new Audio(`./assets/sounds/${event.target.dataset.audio}.mp3`);
-     
+    const button = event.currentTarget;
+    const audio = new Audio(`./assets/sounds/${button.dataset.audio}.mp3`);    
+    
     if (currentAudio !== null) {
       currentAudio.pause();
       currentAudio.currentTime = 0;
@@ -14,8 +17,16 @@ const getSound = (event) => {
       
     audio.play();
     currentAudio = audio;
+
+    button.style.backgroundPosition = '-214px -11px';
 };
 
+const changePosition = (event) => {
+    const button = event.currentTarget;
+    button.style.backgroundPosition = '-5px -11px'
+}
+
 buttons.forEach(button => {
-  button.addEventListener('click', getSound);
+  button.addEventListener('mousedown', getSound);
+  button.addEventListener('mouseup', changePosition);
 });
